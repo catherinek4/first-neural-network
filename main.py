82351f90dfeaaff40b10798f4769f45d1d8620b5
catherinek4 <61625720+catherinek4@users.noreply.github.com>
2020-04-12 16:30:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,28 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+### Displaying the image###
+index = 150
+# plt.imshow(train_set_x_orig[index])
+# plt.show()
+
+# Printing it's label
+print("y =", train_set_y[:, index], "it's a ",
+      classes[np.squeeze(train_set_y[:, index])].decode("utf-8"), "picture")
+
+m_train = len(train_set_x_orig)
+m_test = len(test_set_x_orig)
+num_px = len(train_set_x_orig[0])
+
+print("Number of training examples: m_train = " + str(m_train))
+print("Number of testing examples: m_test = " + str(m_test))
+print("Height/Width of each image: num_px = " + str(num_px))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_set_x shape: " + str(train_set_x_orig.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x shape: " + str(test_set_x_orig.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
