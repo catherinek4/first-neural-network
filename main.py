86a0ade11003528546586d92480d378664903543
catherinek4 <61625720+catherinek4@users.noreply.github.com>
2020-04-13 11:55:52 +0300

    model

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
 print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
 
-train_set_x = train_set_x_flatten / 255
-test_set_x = test_set_x_flatten / 255
+train_set_x = train_set_x_flatten / 255.
+test_set_x = test_set_x_flatten / 255.
 
 
 def sigmoid(z):
@@ -84,3 +84,43 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
     grads = {"dw": dw, "db": db}
     params = {"w": w, "b": b}
     return params, grads, costs
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X)+b)
+    for i in range(m):
+        if A[:, i] >= 0.5:
+            Y_prediction[:, i] = 1
+        else:
+            Y_prediction[:, i] = 0
+    return Y_prediction
+
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations=20000, learning_rate=0.5, print_cost=False):
+    w, b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, cost = optimize(
+        w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    print("train accuracy: {} %".format(
+        100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(
+        100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    d = {"costs": cost,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
+
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y,
+          num_iterations=2000, learning_rate=0.005, print_cost=False)
